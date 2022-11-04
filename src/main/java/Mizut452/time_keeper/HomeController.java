0c13ac2db0aa42810e03b6dd351a254196ec1ea9
M1zut4 <ssvror444@gmail.com>
2022-11-04 16:51:40 +0900

    HomeControllerの作成

--- src/main/java/Mizut452/time_keeper/HomeController.java
+++ src/main/java/Mizut452/time_keeper/HomeController.java
@@ -1,4 +1,24 @@
 package Mizut452.time_keeper;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/mypage/{username}")
+    public String userPage(@PathVariable("username") String username) {
+        return "userpage";
+    }
 }
