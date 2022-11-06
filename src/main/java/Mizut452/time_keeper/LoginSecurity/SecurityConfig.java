df10e5fe24fb8fb1bb0a6f0ce7779939629d80c5
M1zut4 <ssvror444@gmail.com>
2022-11-06 17:57:57 +0900

    アカウント作成機構の作成 SecurityConfig

---
+++ src/main/java/Mizut452/time_keeper/LoginSecurity/SecurityConfig.java
@@ -0,0 +1,48 @@
+package Mizut452.time_keeper.LoginSecurity;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securiyFilterChain(HttpSecurity http) throws Exception{
+        http.formLogin( login -> login
+                .loginProcessingUrl("/login")
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .failureUrl("login?error")
+                .permitAll()
+        ).logout(logout -> logout
+                .logoutSuccessUrl("/")
+        ).authorizeHttpRequests(authz -> authz
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                .permitAll()
+                .mvcMatchers("/")
+                .permitAll()
+                .mvcMatchers("/login")
+                .permitAll()
+                .mvcMatchers("createaccount")
+                .permitAll()
+                .mvcMatchers("/add")
+                .permitAll()
+                .mvcMatchers("/userlist")
+                .permitAll()
+                .anyRequest()
+                .permitAll()
+        );
+        http.csrf().disable();
+        return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
