8d5e3191047e3a20f04b1c8acbd853c7751ce400
M1zut4 <ssvror444@gmail.com>
2022-12-12 22:00:15 +0900

    testの実装1(selectAllのテスト)

--- src/test/java/Mizut452/time_keeper/TimeKeeperApplicationTests.java
+++ src/test/java/Mizut452/time_keeper/TimeKeeperApplicationTests.java
@@ -1,13 +1,24 @@
 package Mizut452.time_keeper;
 
+import Mizut452.time_keeper.Mapper.LoginUserMapperTest;
+import Mizut452.time_keeper.Model.Entity.LoginUserTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class TimeKeeperApplicationTests {
+	@Autowired
+	LoginUserMapperTest loginUserMapperTest;
 
 	@Test
 	void contextLoads() {
+		List<LoginUserTest> loginUserTests = loginUserMapperTest.selectAll();
+		assertEquals(2, loginUserTests.size());
 	}
 
 }
